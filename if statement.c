d6e33a096b67bc6174b36bc2d9327163b673bf0e
zeelan123 <zeelanbasha578@gmail.com>
2024-11-26 12:57:51 +0530

    Update if statement.c

--- if statement.c
+++ if statement.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    // Check if the number is positive
+    if (number > 0) {
+        printf("The number is positive.\n");
+    }
+
+    printf("Program ends here.\n");
+
+    return 0;
+}
 
