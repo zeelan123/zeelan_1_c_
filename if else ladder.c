200962cffe93078dddec9566811a41c1a6cebeb2
zeelan123 <zeelanbasha578@gmail.com>
2024-11-26 12:44:02 +0530

    Update if else ladder.c

--- if else ladder.c
+++ if else ladder.c
@@ -1,1 +1,16 @@
 
+#include <stdio.h>
+
+int main() {
+    int score = 75;
+    if (score >= 90) {
+        printf("Grade: A\n");
+    } else if (score >= 75) {
+        printf("Grade: B\n");
+    } else if (score >= 50) {
+        printf("Grade: C\n");
+    } else {
+        printf("Grade: F\n");
+    }
+    return 0;
+}
