9c880bf9c018869a76bcb51c77681572bd3cc893
zeelan123 <zeelanbasha578@gmail.com>
2024-11-26 14:53:09 +0530

    Update while loop2.c

--- while loop2.c
+++ while loop2.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int n,i=1,sum=0;
+  printf("enter the value of n:");
+  scanf("%d",&n);
+  while(i<=n)
+    {
+      sum+=1;
+      i++;
+      
+    }
+printf("sum of first%d natural numbers is %d"\n ")
 
+return 0;
+}
