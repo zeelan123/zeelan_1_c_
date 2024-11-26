2d8998c575833b47c192c6c36fb185a881ac5712
zeelan123 <zeelanbasha578@gmail.com>
2024-11-26 12:50:40 +0530

    Update if else statement.c

--- if else statement.c
+++ if else statement.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        printf("x is positive.\n");
+    } else {
+        printf("x is not positive.\n");
+    }
+    return 0;
+}
 
