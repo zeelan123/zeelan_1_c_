531c21a8128e9dc15e1ae2e1f58d5c0fa71d80b2
zeelan123 <zeelanbasha578@gmail.com>
2024-11-26 12:40:28 +0530

    Update evan or odd.c

--- evan or odd.c
+++ evan or odd.c
@@ -1,1 +1,17 @@
 
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("The number is even.\n");
+        if (num > 10) {
+            printf("The number is greater than 10.\n");
+        }
+    } else {
+        printf("The number is odd.\n");
+    }
+    return 0;
+}
