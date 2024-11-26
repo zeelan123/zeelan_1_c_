e893af37bec63296fe8082934baa2aeb3f6b676a
zeelan123 <zeelanbasha578@gmail.com>
2024-11-26 12:37:12 +0530

    Update addition of two num.c

--- addition of two num.c
+++ addition of two num.c
@@ -1,1 +1,12 @@
 
+include <stdio.h>
+
+int main(void) {
+  int a,b,c;
+  printf("zeelan basha\n");
+  printf("enter the value of a and b\n");
+scanf ("%d%d",&a,&b);
+  c=a+b;
+  printf("addition of a and b is %d,c");
+  return 0;
+}
