6512bc7b33926b6489cd0d9c182b0e57810ddc4b
zeelan123 <zeelanbasha578@gmail.com>
2024-11-26 15:51:41 +0530

    Update multiplication_table.c

--- multiplication_table.c
+++ multiplication_table.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h> 
+int main()
+{
+  int num;
+  printf("enter a number:");
+  scanf("%d",&num);
+  for(int i=1;i<=10;i++){
+    
+  
+printf("%d x %d=%d\n",num,i,num*i);
+}
+return 0;
+}
