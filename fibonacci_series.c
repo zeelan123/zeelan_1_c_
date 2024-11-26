aaef18f56c34307f29a92de852df447e91e83b12
zeelan123 <zeelanbasha578@gmail.com>
2024-11-26 16:40:31 +0530

    Update fibonacci_series.c

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h> 
+int main()
+{
+  int n,t1=0,t2=1,nextterm;
+  printf("enter the number of terms:");
+  scanf("%d",&n);
+printf("fibonacci series:%d,%d",t1,t2);
+for(int i=3;i<=n;i++)
+  {
+    nextterm=t1+t2;
+    printf("%d",nextterm);
+    t1=t2;
+    t2=nextterm;
+    
+  }
+  
+   
+return 0;
+}
