69c431fce535f78637c8849afee6b6ac053a5d8e
zeelan123 <zeelanbasha578@gmail.com>
2024-11-27 21:26:06 +0530

    Factorial.c

--- Factorial.c
+++ Factorial.c
@@ -1,1 +1,17 @@
-
+include<stdio.h>
+int factorial(int n)
+{ 
+    if(n==0)
+{    
+return 1;
+}
+return n*factorial(n-1);
+}
+int main()
+{
+int num;
+printf(" enter a number:");
+scanf("%d",&num);
+printf("factorial  of =%d\n",factorial(num));
+return 0;
+}
