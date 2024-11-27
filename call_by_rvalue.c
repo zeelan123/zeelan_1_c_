8b31e5befe5b933f19c14b42b5ed2fa27fde9cf6
zeelan123 <zeelanbasha578@gmail.com>
2024-11-27 10:49:22 +0530

    Update call_by_rvalue.c

--- call_by_rvalue.c
+++ call_by_rvalue.c
@@ -1,1 +1,15 @@
 
+#include<stdio.h>
+void swap(int*x,int*y)
+{
+  int temp=*x;
+  *x=*y;
+  *y=temp;
+}
+int main()
+{
+  int a=5,b=10;
+  swap(&a,&b);
+  printf("a=%d,b=%d\n",a,b);
+  return 0;
+}
