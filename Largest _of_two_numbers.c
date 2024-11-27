1a0e4a618d529feed67cb7dc45df18670f320c91
zeelan123 <zeelanbasha578@gmail.com>
2024-11-27 21:29:05 +0530

    Update Largest _of_two_numbers.c

--- Largest _of_two_numbers.c
+++ Largest _of_two_numbers.c
@@ -1,1 +1,16 @@
 
+#include<stdio.h>
+int largest (int a,int b,int c)
+{
+    if(a>=b&&a>=c) return a;
+    if(b>=a&&b>=c) return b;
+    return c;
+}
+int main()
+{ 
+    int x,y,z;
+    printf("enter three numbers ");
+    scanf("%d%d%d",&x,&y,&z);
+  printf("largest=%d\n",largest (x,y,z));
+    return 0;
+}
