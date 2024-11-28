a1e08dc4a2e49bf9954e25cf50d63a9fa1494ae3
zeelan123 <zeelanbasha578@gmail.com>
2024-11-28 12:56:49 +0530

    Update Palindrome.c

--- Palindrome.c
+++ Palindrome.c
@@ -1,1 +1,23 @@
-
+#include<stdio.h>
+int main()
+{
+ int num,reversed=0,reminder,original;
+   printf("enter a integer:") ;
+   scanf("%d",&num);
+   original=num;
+   while (num !=0)
+   {
+       reminder=num %10;
+   reversed=reversed *10 + reminder ;
+   num = num / 10;
+   }
+   if(original==reversed )
+   {
+   printf("%d is a polindrome.\n", original);
+   }
+   else
+   {
+   printf("%d is not a palindrome.\n", original);
+   }
+   return 0;
+}
