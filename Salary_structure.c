ee8d55c9a84f328f2b11df81bbe97a65a42f1de1
zeelan123 <zeelanbasha578@gmail.com>
2024-11-29 14:36:49 +0530

    Update Salary_structure.c

--- Salary_structure.c
+++ Salary_structure.c
@@ -1,1 +1,39 @@
+include <stdio.h>
+struct Employee
+{
+    char name[50];
+    int id;
+    float salary;
+};
+float calculateAverageSalary(struct Employee employees[], int numEmployees) 
+{
+    float totalSalary = 0.0;
+    for (int i = 0; i < numEmployees; i++) {
+        totalSalary += employees[i].salary;
+    }
+    return totalSalary / numEmployees;
+}
 
+int main()
+{
+    int numEmployees;
+    printf("Enter the number of employees: ");
+    scanf("%d", &numEmployees);
+
+    struct Employee employees[numEmployees];
+
+    for (int i = 0; i < numEmployees; i++) {
+        printf("\nEnter details for employee %d:\n", i + 1);
+        printf("Name: ");
+        scanf("%s", employees[i].name);
+        printf("ID: ");
+        scanf("%d", &employees[i].id);
+        printf("Salary: ");
+        scanf("%f", &employees[i].salary);
+    }
+
+    float averageSalary = calculateAverageSalary(employees, numEmployees);
+    printf("\nThe average salary of the employees is: %.2f\n", averageSalary);
+
+    return 0;
+}
