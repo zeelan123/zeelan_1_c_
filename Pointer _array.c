f2f09ae1237372ce0dfc2af470a0235f5d46bb6f
zeelan123 <zeelanbasha578@gmail.com>
2024-11-29 15:04:23 +0530

    Update Pointer _array.c

--- Pointer _array.c
+++ Pointer _array.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    int *ptr = arr; 
+
+    printf("Array elements using pointer:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Element %d: %d\n", i + 1, *(ptr + i));
+    }
+
+    return 0;
+}
