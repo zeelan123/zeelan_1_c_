151149a7f28fcd736e8f67d1b004978fd48255c4
zeelan123 <zeelanbasha578@gmail.com>
2024-11-29 15:03:03 +0530

    Update Pointer_address.c

--- Pointer_address.c
+++ Pointer_address.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    int num = 10;
+    int *ptr = &num;  
+
+    printf("Value of num: %d\n", num);
+    printf("Address of num: %p\n", ptr);
+    printf("Value pointed to by ptr: %d\n", *ptr);
+
+    *ptr = 20;  
+    printf("New value of num: %d\n", num);
+
+    return 0;
+}
