5cf388cf72b1a9cf8dfae3ee1edc681f3bf24967
zeelan123 <zeelanbasha578@gmail.com>
2024-11-29 15:19:10 +0530

    Update Array_elements.c

--- Array_elements.c
+++ Array_elements.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main() {
+    int n, target, count = 0;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to count: ");
+    scanf("%d", &target);
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            count++;
+        }
+    }
+
+    printf("The element %d occurs %d times in the array.\n", target, count);
+    return 0;
+}
