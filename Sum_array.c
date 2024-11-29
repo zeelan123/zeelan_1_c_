3b68ba04b77184b7366651b36dc59594201a9115
zeelan123 <zeelanbasha578@gmail.com>
2024-11-29 15:06:25 +0530

    Update Sum_array.c

--- Sum_array.c
+++ Sum_array.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() {
+    int n, sum = 0;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    printf("Sum of array elements: %d\n", sum);
+    return 0;
+}
