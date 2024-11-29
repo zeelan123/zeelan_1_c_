19f2e1575a0ae23db3a8d7d2e8653c96785315d0
zeelan123 <zeelanbasha578@gmail.com>
2024-11-29 10:34:45 +0530

    Update Structures.c

--- Structures.c
+++ Structures.c
@@ -1,1 +1,20 @@
-
+#include<stdio.h>
+struct student 
+{
+    char name [50];
+    int roll;
+    float marks;
+};
+int main()
+{
+    struct student s1;
+    printf("enter name ");
+    scanf("%s",s1.name);
+    printf("enter roll number");
+    scanf("%d",&s1.roll);
+    printf("enter marks");
+    scanf("%f",&s1.marks);
+    printf("\nstudent details:\n");
+   printf("name:%s\nroll:%d\nmarks:%.2f\n",s1.name,s1.roll,s1.marks);
+   return 0;
+}
